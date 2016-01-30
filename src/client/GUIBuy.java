76303e5b8f7395f75d52f3dd958801ad0fe30e0b
JD <jamesdisenhart@gmail.com>
2016-01-29 19:40:16 -0700

    Buy GUI updates

--- src/client/GUIBuy.java
+++ src/client/GUIBuy.java
@@ -8,9 +8,9 @@ import java.awt.*;
  */
 public class GUIBuy {
 
-    JPanel base, cOHP, assetsP, stocksP;
-    JLabel cOHLab, assetsLab, stocksLab;
-    Font mHeading = new Font("Trebuchet MS", Font.PLAIN, 16);
+    static JPanel base, cOHP, assetsP, stocksP;
+    static JLabel cOHLab, assetsLab, stocksLab;
+    static Font mHeading = new Font("Trebuchet MS", Font.PLAIN, 16);
 
     public JPanel buyPanel() {
         base = new JPanel();
@@ -34,7 +34,7 @@ public class GUIBuy {
         base.add(cOHP);
 
         //Assets Panel
-        Font mHeading = new Font("Trebuchet MS", Font.PLAIN, 16);
+
         assetsP = new JPanel();
         assetsP.setLayout(null);
         assetsP.setSize(300, 50);
@@ -68,7 +68,41 @@ public class GUIBuy {
         return base;
     }
     public static void updateBuy() {
+        base.remove(cOHP);
+        base.remove(assetsP);
 
+        cOHP = new JPanel();
+        cOHP.setLayout(null);
+        cOHP.setSize(200, 50);
+        cOHP.setLocation(50, 25);
+        cOHP.setBackground(new Color(198, 240, 198));
+        cOHLab = new JLabel("Cash On Hand: $" + Score.cashOnHand);
+        cOHLab.setFont(mHeading);
+        cOHLab.setSize(200, 50);
+        cOHLab.setHorizontalAlignment(SwingConstants.CENTER);
+        cOHLab.setVerticalAlignment(SwingConstants.CENTER);
+        cOHLab.setLocation(0, 0);
+        cOHP.add(cOHLab);
+        base.add(cOHP);
+
+        //Assets Panel
+        Font mHeading = new Font("Trebuchet MS", Font.PLAIN, 16);
+        assetsP = new JPanel();
+        assetsP.setLayout(null);
+        assetsP.setSize(300, 50);
+        assetsP.setLocation(275, 25);
+        assetsP.setBackground(new Color(198, 240, 198));
+        assetsLab = new JLabel("Assets: $" + Score.assets + "   Net Change: $ " + NetChangeOfAssets.getNetChange() + " Assets: NA");
+        assetsLab.setFont(mHeading);
+        assetsLab.setSize(300, 50);
+        assetsLab.setHorizontalAlignment(SwingConstants.CENTER);
+        assetsLab.setVerticalAlignment(SwingConstants.CENTER);
+        assetsLab.setLocation(0, 0);
+        assetsP.add(assetsLab);
+        base.add(assetsP);
+
+        base.revalidate();
+        base.repaint();
     }
 
 }
